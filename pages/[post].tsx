fcb10964b03368c183d353fd0a9942e0ae39f339
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-10 12:27:02 +0900

    feat: 포스팅 경로 불러오기 구현

--- pages/[post].tsx
+++ pages/[post].tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from 'next';
+import fs from 'fs';
+import path from 'path';
 
-const Post: NextPage = () => {
+const Post: NextPage<{ posts: any }> = ({ posts }) => {
+  console.log('posts', posts);
+
   return (
     <div>
       <main></main>
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  const _root = process.cwd();
+  const FOLDER_NAME = '__posts';
+  const _postsDir = path.join(_root, FOLDER_NAME);
+  const posts = fs.readdirSync(_postsDir, 'utf-8');
+  const paths = posts.map((filename) => ({
+    params: { post: filename.replace(/\.md/, '') },
+  }));
+
+  return { paths, fallback: false };
+}
 
 export default Post;
