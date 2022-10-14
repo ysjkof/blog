15206f2fab9d33da9863ca46394fafa26ce763a1
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-14 16:32:38 +0900

    feat: Loading 컴포넌트 구현

---
+++ components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed flex items-center justify-center">
+      <span className="animate-pulse">loading...</span>
+    </div>
+  );
+}
