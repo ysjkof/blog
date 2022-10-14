a1e840f52ae186275a8bddfdde4978d8fb46da1e
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-14 15:05:04 +0900

    chore: posts.utils 주석 작성

--- utils/posts.utils.ts
+++ utils/posts.utils.ts
@@ -9,12 +9,19 @@ function getPostsPath() {
   return path.join(rootDir, FOLDER_NAME);
 }
 
+/**
+ * @returns posts 폴더의 모든 파일들
+ */
 export function getPostFiles() {
   const postsPath = getPostsPath();
   const posts = fs.readdirSync(postsPath, 'utf-8');
   return posts;
 }
 
+/**
+ * @param filename 확장자가 포함된 파일 이름
+ * @returns posts 폴더의 해당 파일의 전체 경로
+ */
 export function getPostFile(filename: string) {
   const postsPath = getPostsPath();
   const postPath = path.join(postsPath, filename);
@@ -22,6 +29,9 @@ export function getPostFile(filename: string) {
   return postFile;
 }
 
+/**
+ * @param postFiles 전체 경로가 아닌 확장자가 포함된 파일 이름.
+ */
 export function getMetadata(postFiles: string[]) {
   return postFiles
     .map((postFile) => {
