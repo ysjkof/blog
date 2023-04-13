55363cad178fa4873513a5843ece679badd4b350
seongjin <ysjkof@gmail.com>
2023-04-13 14:57:24 +0900

    chore: Setup Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import { SWRConfig } from 'swr';
 import '../styles/globals.css';
 import '../styles/github.css';
 import GlobalNavigationBar from '../components/GlobalNavigationBar';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 function MyApp({ Component, pageProps }: AppProps<{ fallback: any }>) {
   const { fallback } = pageProps;
 
   return (
     <>
+      <GoogleAnalytics />
       <Head>
         <title>Blog</title>
         <meta name="description" content="Web Developer Personal Blog" />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,17 @@ function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {/* Necessary to prevent error: window.gtag is not defined for Next.js-hydration */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+        />
       </body>
     </Html>
   );
